956a0f384d56b5da0a5e79c57a49087faa045563
Anouck Galoppin <anouck.galop@hotmail.com>
2025-01-08 20:48:01 +0100

    add space between text and button

--- deleteProfile.php
+++ deleteProfile.php
@@ -35,7 +35,7 @@ if (isset($_POST['deleteUser'])) {
     <main>
         <form action="deleteProfile.php" method="post">
             <h1>Are you sure you want to delete your profile?</h1>
-            <button type="submit" name="deleteUser">Yes I am sure</button>
+            <button type="submit" name="deleteUser">Yes, I'm sure!</button>
         </form>
 
 
