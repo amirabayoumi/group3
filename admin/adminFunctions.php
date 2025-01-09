8ff3345433974f34787d7ac8b7a46fc960841f97
svitlana-sl <svitlana.slobodeniuk@gmail.com>
2025-01-09 23:51:38 +0100

    Enhance user management functions: implement transaction handling for user deletion, add new user creation, and validate input fields in edit user form.

--- admin/adminFunctions.php
+++ admin/adminFunctions.php
@@ -41,11 +41,62 @@ function editUser($id, $firstname, $lastname, $country, $mail, $username, $petna
 
 
 //Deletes a user from the database.
+
 function deleteThisUser($id)
 {
-    $sql = "DELETE FROM user WHERE id = :id";
-    $stmt = connectToDB()->prepare($sql);
-    $stmt->execute(['id' => $id]);
+    $pdo = connectToDB(); 
 
-    return $stmt->rowCount() > 0;
+    try {
+        $pdo->beginTransaction();
+
+        //first: delete the whishlist
+        $sql = "DELETE FROM wishlist WHERE user_id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+
+        //then: delete the user
+        $sql = "DELETE FROM user WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+
+        // fixing transaction
+        $pdo->commit();
+
+        return $stmt->rowCount() > 0;
+    } catch (PDOException $e) {
+        // if an error occured rollback the transaction
+        $pdo->rollBack();
+        echo "Error: " . $e->getMessage();
+        return false;
+    }
+}
+
+function addNewUser($firstname, $lastname, $email, $country, $username, $petname, $password)
+{
+    $db = connectToDB();
+    $sql = "INSERT INTO user (firstname, lastname, mail, country, username, petname, password) 
+            VALUES (:firstname, :lastname, :email, :country, :username, :petname, :password)";
+    $stmt = $db->prepare($sql);
+    return $stmt->execute([
+        ':firstname' => $firstname,
+        ':lastname' => $lastname,
+        ':email' => $email,
+        ':country' => $country,
+        ':username' => $username,
+        ':petname' => $petname,
+        ':password' => md5($password)
+    ]);
+}
+
+//Checks if email or username already exists in DB
+function checkExistingUser($username, $email)
+{
+    $db = connectToDB();
+    $sql = "SELECT COUNT(*) FROM user WHERE username = :username OR mail = :email";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([
+        ':username' => $username,
+        ':email' => $email,
+    ]);
+    return $stmt->fetchColumn() > 0; // true, if user exists
 }
