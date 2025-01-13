d74d06eadabb271741cf0aa89ce3d19630537034
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2025-01-13 13:13:00 +0100

    fix all details page

--- detail.php
+++ detail.php
@@ -1,7 +1,7 @@
 <?php
-$errors = [];
-require("./header.php");
-require_once 'function.inc.php';
+// $errors = [];
+// require("./header.php");
+// require_once 'function.inc.php';
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
@@ -10,20 +10,25 @@ error_reporting(E_ALL);
 
 
 
-if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
-    echo "Invalid product ID.";
-    exit;
-}
+// if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+//     echo "Invalid product ID.";
+//     exit;
+// }
 
 
-$productId = (int)$_GET['id'];
+// $productId = (int)$_GET['id'];
 
 
-$product = getProductByID($productId);
+// $product = getProductByID($productId);
 
-if (!$product) {
-    echo "Product not found.";
-    exit;
+// if (!$product) {
+//     echo "Product not found.";
+//     exit;
+// }
+
+if (isset($_GET['detail'])) {
+    $productId = (int)$_GET['detail'];
+    $product = getProductForDetails($productId);
 }
 ?>
 
@@ -44,7 +49,7 @@ if (!$product) {
     <div class="container">
         <main>
             <section id="productDetails">
-                <h1><?= htmlspecialchars($product['title']); ?></h1>
+                <h1 id="productTitle"><?= htmlspecialchars($product['title']); ?></h1>
                 <img src="<?= htmlspecialchars($product['image']); ?>" alt="<?= htmlspecialchars($product['title']); ?>">
                 <p><strong>Price:</strong> <?= $product['price']; ?> &#8364;</p>
             </section>
@@ -52,14 +57,29 @@ if (!$product) {
                 <h2>Description</h2>
                 <p><?= htmlspecialchars($product['description']); ?></p>
             </aside>
-            <form action="index.php" method="post">
-                <button type="submit" name="addWishItem" value="<?= $product['id']; ?>">
-                    Add to Wishlist &#x2665;
-                </button>
+            <form action="<?= $pageName; ?>?detail=<?= $product['id']; ?>" method="post">
+                <button type="submit" style="visibility: <?php if (!isset($_SESSION['uid'])) {
+                                                                print "hidden;";
+                                                            } ?>" id=" <?php if ($product['user_id'] == $_SESSION['uid']) {
+                                                                            print "takeOffWishItem";
+                                                                        } else {
+                                                                            print "addWishItem";
+                                                                        }
+                                                                        ?>" name="<?php if ($product['user_id'] == $_SESSION['uid']) {
+                                                                                        print "takeOffWishItem";
+                                                                                    } else {
+                                                                                        print "addWishItem";
+                                                                                    }
+                                                                                    ?>" value=" <?= $product['id']; ?>"> <?php if (isset($_SESSION['uid']) && $product['user_id'] == $_SESSION['uid']) {
+                                                                                                                                print "&#x2665;";
+                                                                                                                            } elseif (isset($_SESSION['uid']) && $product['user_id'] != $_SESSION['uid']) {
+                                                                                                                                print " Add &#x2665;";
+                                                                                                                            }
+
+
+                                                                                                                            ?> </button>
+
             </form>
         </main>
     </div>
 </body>
-
-
-<?php require('footer.php'); ?>
